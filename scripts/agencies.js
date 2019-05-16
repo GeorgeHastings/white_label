ceaed74df0d7a5082237a810ed6daf5c2c5eca9e
ebozeman <ebozeman@users.noreply.github.com>
2019-05-16 11:29:11 -0400

    Update agencies.js

--- scripts/agencies.js
+++ scripts/agencies.js
@@ -226,7 +226,7 @@ export const AFI = {
   email: '	service@afi.org',
   phone: '	800-495-8234',
   reviewCopy: 'When it comes to sound business decisions, its important to trust the right people. Armed Forces Insurance, the name military families have trusted with their home, renter, and auto insurance, is proud to offer coverage for your business.',
-  aboutCopy: 'Since 1887, Armed Forces Insurance (AFI) has existed with a single, unwavering purpose: To protect the people who protect our nation. So for more than 130 years, weve provided military homeowner insurance, military renter insurance, military auto insurance and much more to our members.,
+  aboutCopy: 'Since 1887, Armed Forces Insurance (AFI) has existed with a single, unwavering purpose: To protect the people who protect our nation. So for more than 130 years, weve provided military homeowner insurance, military renter insurance, military auto insurance and much more to our members.',
   buttonLink: 'https://georgehastings.github.io/insurance_flow/?agent=andrew'
 };
 
